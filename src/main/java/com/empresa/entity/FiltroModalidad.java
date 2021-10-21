32d18789753d4a9c8d62595f922fc30887ecfd9f
Nil Eduardo Alcalde Cueva <85600276+Nilsito2000@users.noreply.github.com>
2021-10-21 06:09:01 -0500

    me olvide subirlo

---
+++ src/main/java/com/empresa/entity/FiltroModalidad.java
@@ -0,0 +1,20 @@
+package com.empresa.entity;
+
+public class FiltroModalidad {
+	private String nombre;
+	private int idDeporte;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getIdDeporte() {
+		return idDeporte;
+	}
+	public void setIdDeporte(int idDeporte) {
+		this.idDeporte = idDeporte;
+	}
+
+	
+}
--- src/main/java/com/empresa/repository/ModalidadRepository.java
+++ src/main/java/com/empresa/repository/ModalidadRepository.java
@@ -1,10 +1,22 @@
 package com.empresa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.empresa.entity.Docente;
+import com.empresa.entity.FiltroDocente;
+import com.empresa.entity.FiltroModalidad;
 import com.empresa.entity.Modalidad;
 
 public interface ModalidadRepository extends JpaRepository<Modalidad, Integer>  {
 
-	
+
+	@Query("select m from Modalidad m where "
+			
+			+ "( :#{#fil.nombre} is '' or m.nombre like :#{#fil.nombre} ) and "
+			+ "( :#{#fil.idDeporte} is 0  or m.deporte.idDeporte = :#{#fil.idDeporte} ) ")
+	public abstract List<Modalidad> listaPorFiltro(@Param("fil")FiltroModalidad filtro);
 }
